a10f800ed4aeecdf000890693b7868cea0f4a7a8
homola258 <2264736554@qq.com>
2026-03-06 10:50:54 +0800

    添加CH32V307VCT6项目完整代码

---
+++ obj/Core/subdir.mk
@@ -0,0 +1,21 @@
+################################################################################
+# MRS Version: 1.9.2
+# 自动生成的文件。不要编辑！
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Core/core_riscv.c 
+
+OBJS += \
+./Core/core_riscv.o 
+
+C_DEPS += \
+./Core/core_riscv.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Core/%.o: ../Core/%.c
+	@	@	riscv-none-embed-gcc -march=rv32imacxw -mabi=ilp32 -msmall-data-limit=8 -msave-restore -Os -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections -fno-common -Wunused -Wuninitialized  -g -I"C:\Users\雨季树懒\Desktop\项目\门锁\代码\第一讲\CH32V307VCT6\Debug" -I"C:\Users\雨季树懒\Desktop\项目\门锁\代码\第一讲\CH32V307VCT6\Core" -I"C:\Users\雨季树懒\Desktop\项目\门锁\代码\第一讲\CH32V307VCT6\User" -I"C:\Users\雨季树懒\Desktop\项目\门锁\代码\第一讲\CH32V307VCT6\Peripheral\inc" -std=gnu99 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -c -o "$@" "$<"
+	@	@
+
